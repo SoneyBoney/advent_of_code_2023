cc3734480c08c385ed2520ee4457b97197fa2208
Campinghedgehog <campinghedgehog@gmail.com>
2023-12-12 11:53:16 +0800

    day11, pt2 optimization

--- day11/part2.py
+++ day11/part2.py
@@ -13,13 +13,12 @@ def transpose(m):
             m2[j][i] = m[i][j]
     return m2
 
-def count_empty_rows(m, start, end):
-    count = 0
-    for r in range(start,end+1):
-        row = m[r]
+def count_empty_rows(m):
+    ret = []
+    for i, row in enumerate(m):
         if all([x=='.' for x in row]):
-            count+=1
-    return count
+            ret.append(i)
+    return ret
     
 
 
@@ -27,22 +26,23 @@ def manhattan_distance(a,b):
     return abs(b[0]-a[0]) + abs(b[1]-a[1])
 
 def sol(m,index_map):
+    empty_row_list =  count_empty_rows(m)
+    empty_col_list = count_empty_rows(transpose(m))
     ret = 0
     seen = set()
     print(f"num galaxies: {len(index_map.keys())}") 
-    count = 0
     for a,a_val in index_map.items():
         for b,b_val in index_map.items():
             if (b,a) not in seen:
-                print(count)
                 temp = manhattan_distance(a_val,b_val)
-                empty_rows = count_empty_rows(m, min(a_val[0],b_val[0]), max(a_val[0],b_val[0]))
-                empty_cols = count_empty_rows(transpose(m), min(a_val[1],b_val[1]), max(a_val[1],b_val[1])) 
+                min_r,max_r = (min(a_val[0],b_val[0]), max(a_val[0],b_val[0]))
+                min_c,max_c = (min(a_val[1],b_val[1]), max(a_val[1],b_val[1]))
+                empty_rows = len([x for x in empty_row_list if min_r < x and x < max_r])
+                empty_cols = len([x for x in empty_col_list if min_c < x and x < max_c])
                 temp -= empty_rows + empty_cols
                 temp += 1000000*(empty_rows + empty_cols)
                 ret += temp
                 seen.add((a,b))
-                count +=1
     return ret
             
 
